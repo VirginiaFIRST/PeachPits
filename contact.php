8b8c23b441538f5974da5b76422a8d1569b39baa
dcomella <derek@comella.net>
2017-06-19 12:57:52 -0400

    Started a Contact Us page

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php include "header.php"; ?>
+<div class="page-head">
+    <div class="container">
+        <h1>
+            Contact Us
+        </h1>
+    </div>
+</div>
+
+<?php include "footer.php"; ?>
